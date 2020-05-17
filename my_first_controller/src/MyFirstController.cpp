4515712e06c8170768c98ec913a47882f8166f09
Masaki Murooka <m-murooka@aist.go.jp>
2020-05-17 17:11:50 +0900

    add com controller according to tutorials.

--- my_first_controller/src/MyFirstController.cpp
+++ my_first_controller/src/MyFirstController.cpp
@@ -5,11 +5,22 @@ MyFirstController::MyFirstController(mc_rbdyn::RobotModulePtr rm, double dt, con
 {
   config_.load(config);
   solver().addConstraintSet(contactConstraint);
-  solver().addConstraintSet(kinematicsConstraint);
+  solver().addConstraintSet(dynamicsConstraint);
+  // solver().addConstraintSet(kinematicsConstraint);
   solver().addTask(postureTask);
-  solver().setContacts({{}});
+  // solver().setContacts({{}});
+  solver().setContacts({
+      {robots(), 0, 1, "LeftFoot", "AllGround"},
+        {robots(), 0, 1, "RightFoot", "AllGround"}
+    });
 
   jointIndex = robot().jointIndexByName("NECK_Y");
+
+  // In the constructor, create the task and add it to the problem
+  comTask = std::make_shared<mc_tasks::CoMTask>(robots(), 0, 10.0, 1000.0);
+  solver().addTask(comTask);
+  // Reduce the posture task stiffness
+  postureTask->stiffness(1);
 
   LOG_SUCCESS("MyFirstController init done " << this)
 }
@@ -21,12 +32,21 @@ bool MyFirstController::run()
       switch_target();
     }
 
+  if(comTask->eval().norm() < 0.01)
+    {
+      switch_com_target();
+    }
+
   return mc_control::MCController::run();
 }
 
 void MyFirstController::reset(const mc_control::ControllerResetData & reset_data)
 {
   mc_control::MCController::reset(reset_data);
+
+  // In the reset function, reset the task to the current CoM
+  comTask->reset();
+  comZero = comTask->com();
 }
 
 void MyFirstController::switch_target()
@@ -40,6 +60,22 @@ void MyFirstController::switch_target()
       postureTask->target({{"NECK_Y", robot().ql()[jointIndex]}});
     }
   goingLeft = !goingLeft;
+}
+
+void MyFirstController::switch_com_target()
+{
+  // comZero is obtained by doing:
+  // comZero = comTask->com();
+  // in the reset function
+  if(comDown)
+    {
+      comTask->com(comZero - Eigen::Vector3d{0, 0, 0.2});
+    }
+  else
+    {
+      comTask->com(comZero);
+    }
+  comDown = !comDown;
 }
 
 CONTROLLER_CONSTRUCTOR("MyFirstController", MyFirstController)
