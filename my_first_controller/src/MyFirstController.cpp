d7728a93c08f6eedc2ec59b51c526905b958ad03
Masaki Murooka <m-murooka@aist.go.jp>
2020-05-17 12:11:30 +0900

    add neck moving function to my controller according to tutorial.

--- my_first_controller/src/MyFirstController.cpp
+++ my_first_controller/src/MyFirstController.cpp
@@ -9,17 +9,37 @@ MyFirstController::MyFirstController(mc_rbdyn::RobotModulePtr rm, double dt, con
   solver().addTask(postureTask);
   solver().setContacts({{}});
 
+  jointIndex = robot().jointIndexByName("NECK_Y");
+
   LOG_SUCCESS("MyFirstController init done " << this)
 }
 
 bool MyFirstController::run()
 {
+  if(std::abs(postureTask->posture()[jointIndex][0] - robot().mbc().q[jointIndex][0]) < 0.05)
+    {
+      switch_target();
+    }
+
   return mc_control::MCController::run();
 }
 
 void MyFirstController::reset(const mc_control::ControllerResetData & reset_data)
 {
   mc_control::MCController::reset(reset_data);
+}
+
+void MyFirstController::switch_target()
+{
+  if(goingLeft)
+    {
+      postureTask->target({{"NECK_Y", robot().qu()[jointIndex]}});
+    }
+  else
+    {
+      postureTask->target({{"NECK_Y", robot().ql()[jointIndex]}});
+    }
+  goingLeft = !goingLeft;
 }
 
 CONTROLLER_CONSTRUCTOR("MyFirstController", MyFirstController)
